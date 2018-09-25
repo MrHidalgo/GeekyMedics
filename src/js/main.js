4ea214b8fdd322b4a6f3655a9a254f7be2121436
Sergey Khmelevskoy <xs290@me.com>
2018-09-25 04:16:02 +0300

    up

--- src/js/main.js
+++ src/js/main.js
@@ -6,62 +6,59 @@ $(document).ready(function(){
 
   var _window = $(window);
   var _document = $(document);
-
-  // BREAKPOINT SETTINGS
-  var bp = {
-    mobileS: 375,
-    mobile: 568,
-    tablet: 768,
-    desktop: 992,
-    wide: 1336,
-    hd: 1680
-  };
-
+  var lastClickEl;
   var easingSwing = [.02, .01, .47, 1]; // default jQuery easing for anime.js
 
   ////////////
   // READY - triggered when PJAX DONE
   ////////////
+
+  // single time initialization
+  legacySupport();
+  // setTimeout(initAOS, 500);
+  initAOS();
+  initPreloader();
+
+  // on transition change
   function pageReady(){
-    legacySupport();
-    // updateHeaderActiveClass();
-    // initHeaderScroll();
-
-    // initPopups();
-    // initSliders();
-    // initScrollMonitor();
-    // initMasks();
-    // initLazyLoad();
-
-    // development helper
-    _window.on('resize', debounce(setBreakpoint, 200));
-
-    // AVAILABLE in _components folder
-    // copy paste in main.js and initialize here
-
-    // initTeleport();
-    // parseSvg();
-    // revealFooter();
-    // _window.on('resize', throttle(revealFooter, 100));
-
     changeImageSVG();
     titleCollapse();
     circleSvgProgress();
+    closeAll();
   }
 
   // this is a master function which should have all functionality
   pageReady();
 
 
-  // some plugins work best with onload triggers
-  _window.on('load', function(){
-    // your functions
-  });
-
-
   //////////
   // COMMON
   //////////
+
+  function initAOS() {
+    AOS.init();
+  }
+
+  function initPreloader(){
+    var $overlay = $('.js-transition-overlay');
+
+    TweenLite.fromTo($overlay, 1, {
+        x: "0%",
+        overwrite: "all"
+      },
+      {
+        x: "100%",
+        ease: Expo.easeOut,
+        delay: 0.2,
+        onComplete: function() {
+          $overlay.remove();
+          $("body").removeClass("is-transitioning");
+        }
+      }
+    );
+
+
+  }
 
   function legacySupport(){
     // svg support for laggy browsers
@@ -75,21 +72,60 @@ $(document).ready(function(){
     });
   }
 
+  function closeAll(){
+    closeUserDropdown();
+  }
+
 
   // Prevent # behavior
-	_document
-    .on('click', '[href="#"]', function(e) {
-  		e.preventDefault();
-  	})
-    .on('click', 'a[href^="#section"]', function() { // section scroll
-      var el = $(this).attr('href');
-      $('body, html').animate({
-          scrollTop: $(el).offset().top}, 1000);
+  _document
+    .on("click", '[href="#"]', function(e) {
+      e.preventDefault();
+    })
+    .on("click", "a[href]", function(e) {
+      if (Barba.Pjax.transitionProgress) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+
+      if (e.currentTarget.href === window.location.href) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    })
+    .on("click", 'a[href^="#section"]', function(e) {
+      // section scroll
+      var el = $(this).attr("href");
+      scrollToSection($(el));
       return false;
     });
 
+  function scrollToSection(el) {
+    var headerHeight = $(".header").height();
+    var targetScroll = el.offset().top - headerHeight;
 
-// .img-svg
+    TweenLite.to(window, 1, {
+      scrollTo: targetScroll,
+      ease: easingSwing
+    });
+  }
+
+
+  /////////
+  // header dropdown
+  /////////
+  _document.on('click', '[js-header-dropdown]', function(e){
+    $(this).toggleClass('is-active');
+  })
+
+
+  function closeUserDropdown(){
+    $('[js-header-dropdown]').removeClass('is-active');
+  }
+
+
+
+  // .img-svg
   function changeImageSVG() {
     document.querySelectorAll('img.svg-js').forEach(function (element) {
       var imgID = element.getAttribute('id'),
@@ -163,235 +199,6 @@ $(document).ready(function(){
     });
   }
 
-  // HEADER SCROLL
-  // add .header-static for .page or body
-  // to disable sticky header
-  // function initHeaderScroll(){
-  //   _window.on('scroll', throttle(function(e) {
-  //     var vScroll = _window.scrollTop();
-  //     var header = $('.header').not('.header--static');
-  //     var headerHeight = header.height();
-  //     var firstSection = _document.find('.page__content div:first-child()').height() - headerHeight;
-  //     var visibleWhen = Math.round(_document.height() / _window.height()) >  2.5
-  //
-  //     if (visibleWhen){
-  //       if ( vScroll > headerHeight ){
-  //         header.addClass('is-fixed');
-  //       } else {
-  //         header.removeClass('is-fixed');
-  //       }
-  //       if ( vScroll > firstSection ){
-  //         header.addClass('is-fixed-visible');
-  //       } else {
-  //         header.removeClass('is-fixed-visible');
-  //       }
-  //     }
-  //   }, 10));
-  // }
-
-
-  // HAMBURGER TOGGLER
-  // _document.on('click', '[js-hamburger]', function(){
-  //   $(this).toggleClass('is-active');
-  //   $('.mobile-navi').toggleClass('is-active');
-  // });
-
-  // function closeMobileMenu(){
-  //   $('[js-hamburger]').removeClass('is-active');
-  //   $('.mobile-navi').removeClass('is-active');
-  // }
-
-  // SET ACTIVE CLASS IN HEADER
-  // * could be removed in production and server side rendering when header is inside barba-container
-  // function updateHeaderActiveClass(){
-  //   $('.header__menu li').each(function(i,val){
-  //     if ( $(val).find('a').attr('href') == window.location.pathname.split('/').pop() ){
-  //       $(val).addClass('is-active');
-  //     } else {
-  //       $(val).removeClass('is-active')
-  //     }
-  //   });
-  // }
-
-  //////////
-  // SLIDERS
-  //////////
-
-  // function initSliders(){
-  //   var slickNextArrow = '<div class="slick-prev"><svg class="ico ico-back-arrow"><use xlink:href="img/sprite.svg#ico-back-arrow"></use></svg></div>';
-  //   var slickPrevArrow = '<div class="slick-next"><svg class="ico ico-next-arrow"><use xlink:href="img/sprite.svg#ico-next-arrow"></use></svg></div>'
-  //
-  //   // General purpose sliders
-  //   $('[js-slider]').each(function(i, slider){
-  //     var self = $(slider);
-  //
-  //     // set data attributes on slick instance to control
-  //     if (self && self !== undefined) {
-  //       self.slick({
-  //         autoplay: self.data('slick-autoplay') !== undefined ? true : false,
-  //         dots: self.data('slick-dots') !== undefined ? true : false,
-  //         arrows: self.data('slick-arrows') !== undefined ? true : false,
-  //         prevArrow: slickNextArrow,
-  //         nextArrow: slickPrevArrow,
-  //         infinite: self.data('slick-infinite') !== undefined ? true : true,
-  //         speed: 300,
-  //         slidesToShow: 1,
-  //         accessibility: false,
-  //         adaptiveHeight: true,
-  //         draggable: self.data('slick-no-controls') !== undefined ? false : true,
-  //         swipe: self.data('slick-no-controls') !== undefined ? false : true,
-  //         swipeToSlide: self.data('slick-no-controls') !== undefined ? false : true,
-  //         touchMove: self.data('slick-no-controls') !== undefined ? false : true
-  //       });
-  //     }
-  //
-  //   })
-  //
-  //   // other individual sliders goes here
-  //   $('[js-myCustomSlider]').slick({
-  //
-  //   })
-  //
-  // }
-
-  //////////
-  // MODALS
-  //////////
-
-  // function initPopups(){
-  //   // Magnific Popup
-  //   var startWindowScroll = 0;
-  //   $('[js-popup]').magnificPopup({
-  //     type: 'inline',
-  //     fixedContentPos: true,
-  //     fixedBgPos: true,
-  //     overflowY: 'auto',
-  //     closeBtnInside: true,
-  //     preloader: false,
-  //     midClick: true,
-  //     removalDelay: 300,
-  //     mainClass: 'popup-buble',
-  //     callbacks: {
-  //       beforeOpen: function() {
-  //         startWindowScroll = _window.scrollTop();
-  //         // $('html').addClass('mfp-helper');
-  //       },
-  //       close: function() {
-  //         // $('html').removeClass('mfp-helper');
-  //         _window.scrollTop(startWindowScroll);
-  //       }
-  //     }
-  //   });
-  //
-  //   $('[js-popup-gallery]').magnificPopup({
-  // 		delegate: 'a',
-  // 		type: 'image',
-  // 		tLoading: 'Загрузка #%curr%...',
-  // 		mainClass: 'popup-buble',
-  // 		gallery: {
-  // 			enabled: true,
-  // 			navigateByImgClick: true,
-  // 			preload: [0,1]
-  // 		},
-  // 		image: {
-  // 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-  // 		}
-  // 	});
-  // }
-  //
-  // function closeMfp(){
-  //   $.magnificPopup.close();
-  // }
-
-  ////////////
-  // UI
-  ////////////
-
-  // textarea autoExpand
-  // _document
-  //   .one('focus.autoExpand', '.ui-group textarea', function(){
-  //       var savedValue = this.value;
-  //       this.value = '';
-  //       this.baseScrollHeight = this.scrollHeight;
-  //       this.value = savedValue;
-  //   })
-  //   .on('input.autoExpand', '.ui-group textarea', function(){
-  //       var minRows = this.getAttribute('data-min-rows')|0, rows;
-  //       this.rows = minRows;
-  //       rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-  //       this.rows = minRows + rows;
-  //   });
-
-  // Masked input
-  // function initMasks(){
-  //   $("[js-dateMask]").mask("99.99.99",{placeholder:"ДД.ММ.ГГ"});
-  //   $("input[type='tel']").mask("+7 (000) 000-0000", {placeholder: "+7 (___) ___-____"});
-  // }
-
-
-  ////////////
-  // SCROLLMONITOR - WOW LIKE
-  ////////////
-  // function initScrollMonitor(){
-  //   $('.wow').each(function(i, el){
-  //
-  //     var elWatcher = scrollMonitor.create( $(el) );
-  //
-  //     var delay;
-  //     if ( $(window).width() < 768 ){
-  //       delay = 0
-  //     } else {
-  //       delay = $(el).data('animation-delay');
-  //     }
-  //
-  //     var animationClass = $(el).data('animation-class') || "wowFadeUp"
-  //
-  //     var animationName = $(el).data('animation-name') || "wowFade"
-  //
-  //     elWatcher.enterViewport(throttle(function() {
-  //       $(el).addClass(animationClass);
-  //       $(el).css({
-  //         'animation-name': animationName,
-  //         'animation-delay': delay,
-  //         'visibility': 'visible'
-  //       });
-  //     }, 100, {
-  //       'leading': true
-  //     }));
-  //     elWatcher.exitViewport(throttle(function() {
-  //       $(el).removeClass(animationClass);
-  //       $(el).css({
-  //         'animation-name': 'none',
-  //         'animation-delay': 0,
-  //         'visibility': 'hidden'
-  //       });
-  //     }, 100));
-  //   });
-  //
-  // }
-
-
-  //////////
-  // LAZY LOAD
-  //////////
-  // function initLazyLoad(){
-  //   _document.find('[js-lazy]').Lazy({
-  //     threshold: 500,
-  //     enableThrottle: true,
-  //     throttle: 100,
-  //     scrollDirection: 'vertical',
-  //     effect: 'fadeIn',
-  //     effectTime: 350,
-  //     // visibleOnly: true,
-  //     // placeholder: "data:image/gif;base64,R0lGODlhEALAPQAPzl5uLr9Nrl8e7...",
-  //     onError: function(element) {
-  //         console.log('error loading ' + element.data('src'));
-  //     },
-  //     beforeLoad: function(element){
-  //       // element.attr('style', '')
-  //     }
-  //   });
-  // }
 
   //////////
   // BARBA PJAX
@@ -399,101 +206,112 @@ $(document).ready(function(){
 
   Barba.Pjax.Dom.containerClass = "page";
 
-  var FadeTransition = Barba.BaseTransition.extend({
+  var OverlayTransition = Barba.BaseTransition.extend({
     start: function() {
-      Promise
-        .all([this.newContainerLoading, this.fadeOut()])
-        .then(this.fadeIn.bind(this));
+      Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+        this.fadeIn.bind(this)
+      );
     },
 
     fadeOut: function() {
       var deferred = Barba.Utils.deferred();
 
-      anime({
-        targets: this.oldContainer,
-        opacity : .5,
-        easing: easingSwing, // swing
-        duration: 300,
-        complete: function(anim){
-          deferred.resolve();
-        }
-      })
+      // store overlay globally to access in fadein
+      this.$overlay = $('<div class="js-transition-overlay"></div>');
+      this.$overlay.insertAfter(".header");
+      $("body").addClass("is-transitioning");
 
-      return deferred.promise
+      TweenLite.fromTo(this.$overlay, 0.6,
+        {
+          x: "0%"
+        },
+        {
+          x: "100%",
+          ease: Quart.easeIn,
+          onComplete: function() {
+            deferred.resolve();
+          }
+        }
+      );
+
+      return deferred.promise;
     },
 
     fadeIn: function() {
-      var _this = this;
+      var _this = this; // copy to acces inside animation callbacks
       var $el = $(this.newContainer);
 
       $(this.oldContainer).hide();
 
       $el.css({
-        visibility : 'visible',
-        opacity : .5
+        visibility: "visible"
       });
 
-      anime({
-        targets: "html, body",
-        scrollTop: 0,
-        easing: easingSwing, // swing
-        duration: 150
+      TweenLite.to(window, 0.2, {
+        scrollTo: 1,
+        ease: easingSwing
       });
 
-      anime({
-        targets: this.newContainer,
-        opacity: 1,
-        easing: easingSwing, // swing
-        duration: 300,
-        complete: function(anim) {
-          triggerBody()
-          _this.done();
+      AOS.refreshHard(); // refresh AOS instance
+
+      TweenLite.fromTo(
+        this.$overlay,
+        1,
+        {
+          x: "100%",
+          overwrite: "all"
+        },
+        {
+          x: "200%",
+          ease: Expo.easeOut,
+          delay: 0.2,
+          onComplete: function() {
+            _this.$overlay.remove();
+            triggerBody();
+            $("body").removeClass("is-transitioning");
+            _this.done();
+          }
         }
-      });
+      );
     }
   });
 
   // set barba transition
   Barba.Pjax.getTransition = function() {
-    return FadeTransition;
+    // return FadeTransition;
+    return OverlayTransition;
   };
 
   Barba.Prefetch.init();
   Barba.Pjax.start();
 
-  Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container, newPageRawHTML) {
+  // event handlers
+  Barba.Dispatcher.on("linkClicked", function(el) {
+    lastClickEl = el; // save last click to detect transition type
+  });
 
+  Barba.Dispatcher.on("initStateChange", function(currentStatus) {
+    var container = Barba.Pjax.Dom.getContainer();
+    var haveContainer = $(container).find(".page__content").length > 0;
+
+    if (!haveContainer) {
+      // handle error - redirect ot page regular way
+      window.location.href = currentStatus.url;
+    }
+  });
+
+  Barba.Dispatcher.on("newPageReady", function(currentStatus, oldStatus, container,newPageRawHTML) {
     pageReady();
-    closeMobileMenu();
+  });
+
+  Barba.Dispatcher.on("transitionCompleted", function() {
 
   });
 
   // some plugins get bindings onNewPage only that way
-  function triggerBody(){
+  function triggerBody() {
     $(window).scroll();
     $(window).resize();
-  }
-
-  //////////
-  // DEVELOPMENT HELPER
-  //////////
-  function setBreakpoint(){
-    var wHost = window.location.host.toLowerCase()
-    var displayCondition = wHost.indexOf("localhost") >= 0 || wHost.indexOf("surge") >= 0
-    if (displayCondition){
-      console.log(displayCondition)
-      var wWidth = _window.width();
-
-      var content = "<div class='dev-bp-debug'>"+wWidth+"</div>";
-
-      $('.page').append(content);
-      setTimeout(function(){
-        $('.dev-bp-debug').fadeOut();
-      },1000);
-      setTimeout(function(){
-        $('.dev-bp-debug').remove();
-      },1500)
-    }
   }
 
 });
